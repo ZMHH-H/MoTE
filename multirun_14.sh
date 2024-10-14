f43e7b6cf56dda70d265c50bc4629a83c5211a18
ZMHH-H <758833971@qq.com>
2024-10-14 20:56:52 +0800

    update

--- multirun_14.sh
+++
@@ -1,9 +0,0 @@
-bash scripts/run_train.sh configs/k400/k400_train_video_vitl-14-f8.yaml
-
-sleep 10s
-
-bash scripts/run_train.sh configs/k400/temp_vitl-14-f8.yaml
-
-sleep 10s
-
-bash scripts/run_train.sh configs/k400/k400_train_video_vitl-14-f8.yaml
--- multirun_16.sh
+++
@@ -1,8 +0,0 @@
-# bash scripts/run_train.sh configs/k400/k400_train_video_vitl-14-f8.yaml
-
-# bash scripts/run_train.sh configs/k400/zmh_k400_train_video_vitb-16-f8.yaml
-
-bash scripts/run_test_zeroshot.sh configs/hmdb51/hmdb_split1.yaml MoTE_B16.pt --test_clips 3
-
-# bash scripts/run_test.sh configs/k400/zmh_k400_train_video_vitb-16-f8.yaml MoTE_B16.pt
-
